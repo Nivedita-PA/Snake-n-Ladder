eff2a30871c0bb16295564f932d0a59d9361f1d7
Nivedita-PA <npal4661@gmail.com>
2021-04-16 21:28:21 +0530

    [Nivedita]: added the die roll option

---
+++ SnakenLadderUC2.java
@@ -0,0 +1,11 @@
+public class SnakenLadderUC2 {
+
+	public static void main(String[] args) {
+		//variables
+		int start_Position = 0;
+              double dice_Roll = (int) Math.floor(Math.random() * 10) % 6;             
+
+             System.out.println(dice_Roll);
+     }
+
+}
